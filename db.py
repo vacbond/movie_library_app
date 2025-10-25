32abda69b829b8388f14e8a0289b5032179e1ad9
Vaclav Bondar <vaclavbondar@gmail.com>
2025-10-25 14:27:11 +0200

    updated db connection script

--- db.py
+++ db.py
@@ -4,12 +4,16 @@ load_dotenv()
 
 import psycopg2  # pyright: ignore[reportMissingModuleSource]
 
-conn = psycopg2.connect(
+
+def get_connection():
+    conn = psycopg2.connect(
     host=os.getenv("DB_HOST"),
     database=os.getenv("DB_NAME"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
-    port=os.getenv("DB_PORT"),
+    port=os.getenv("DB_PORT")
 )
+    cur = conn.cursor()
+    return cur, conn
 
 print("Connection is OK")
